2114f40ebc3ae9318a219e9f89d4cb24ae52fad4
Константин <konstantin.larin.33@gmail.com>
2026-02-18 16:17:45 +0300

    #

--- coderun/98. Треугольники.py
+++ coderun/98. Треугольники.py
@@ -1,34 +1,132 @@
-# iter 3 неправильный ответ и я догадываюсь почему
+# iter 5
+# решение на iter 4 правильное, но  memory limit exceed
+# Может быть, нужно как-то помечать треугольники, 
+# которые мы уже посчитали, но без хранения всех троек? 
+# Или подойти с другой стороны: считать все равнобедренные, а потом вычесть те, которые посчитались лишний раз?
+
+
+
+# iter 4 
+# сейчас протестировал на трех точках на одной прямой и мой код это считает
+# 3
+# 1 1
+# 2 2
+# 3 3
+# 1
+# и действительно - если мы на точке 2 считаем, то код засчитывает эту тройку как треугольник
+# то есть перед окончательным ответом надо отфильтровать коллинеарные точки
+
+# from collections import defaultdict
+# import math
+# n = int(input())
+# points = []
+# for _ in range(n):    
+#     x, y = map(int, input().split())
+#     points.append((x, y))
+
+
+# ans = 0
+# trios = set()
+# for i in range(n):
+#     dists = defaultdict(list)
+#     for j in range(n):
+#         if i == j:
+#             continue
+#         x1, y1 = points[i]
+#         x2, y2 = points[j]                        
+
+#         euclid = pow(x2 - x1, 2) + pow(y2 - y1, 2)
+#         dists[euclid].append(j)
+#     for dist, ps in dists.items():
+#         for z in range(len(ps)):
+#             p1 = ps[z]
+#             for b in range(z+1, len(ps)):
+#                 p2 = ps[b]
+#                 trio = tuple(sorted([i, p1, p2]))                
+#                 trios.add((trio))
+# # считаем вырожденные треугольники 
+# # точки коллинеарны если их векторное произведение равно нулю
+# # ну и площадь их треугольника будет 0
+# # AB = (x1 - x2, y1 - y2)
+# # AC = (x1 - x3, y1 - y3)
+# # AB x AC = (x1 - x2) * (y1 - y3) - (x1 - x3) * (y1 - y2) 
+# ans = 0
+# for trio in trios:
+#     p1, p2, p3 = trio
+#     x1, y1 = points[p1]
+#     x2, y2 = points[p2]
+#     x3, y3 = points[p3]
+#     s = (x1 - x2) * (y1 - y3) - (x1 - x3) * (y1 - y2)
+#     if s != 0:
+#         ans += 1
+# print(ans)
+
+# iter 3 
+# неправильный ответ и я догадываюсь почему
 # если есть равносторонние треугольники, то они считаются дважды
+# но как мне учесть их?
+# from collections import defaultdict
+# import math
+# n = int(input())
+# points = []
+# for _ in range(n):    
+#     x, y = map(int, input().split())
+#     points.append((x, y))
+
+
+# ans = 0
+# # можно по идее записывать тройки в один сет отсортированными кортежами индексов точек, но это нам потребуется 
+# # перебирать все эти точки в ручную, что очень затратно
+# # хотя раз точек всего 1500, то может это простительно - давай попробуем
+# trios = set()
+# for i in range(n):
+#     dists = defaultdict(list)
+#     for j in range(n):
+#         if i == j:
+#             continue
+#         x1, y1 = points[i]
+#         x2, y2 = points[j]                        
+
+#         euclid = pow(x2 - x1, 2) + pow(y2 - y1, 2)
+#         dists[euclid].append(j)
+#     for dist, ps in dists.items():
+#         for z in range(len(ps)):
+#             p1 = ps[z]
+#             for b in range(z+1, len(ps)):
+#                 p2 = ps[b]
+#                 trio = tuple(sorted([i, p1, p2]))                
+#                 trios.add((trio))
+
+# print(len(trios))
 
 # iter 2
 # равнобедренные треугольники можно найти за O(n^2), если для каждой точки посчитать расстояние до других точек и посчитать количество совпавших
 # допустим у точки есть 2 точки расстояние до которых m, значит это один равнобедренный треугольник
 # если 3 то уже 2 треугольника и тд
 # то есть если n точек с одинаковым расстоянием до одной вершины, то их количество это C(n, 2)
-from collections import defaultdict
-import math
-n = int(input())
-points = []
-for _ in range(n):    
-    x, y = map(int, input().split())
-    points.append((x, y))
+# from collections import defaultdict
+# import math
+# n = int(input())
+# points = []
+# for _ in range(n):    
+#     x, y = map(int, input().split())
+#     points.append((x, y))
 
 
-ans = 0
-for i in range(n):
-    dists = defaultdict(int)
-    for j in range(n):
-        if i == j:
-            continue
-        x1, y1 = points[i]
-        x2, y2 = points[j]                        
+# ans = 0
+# for i in range(n):
+#     dists = defaultdict(int)
+#     for j in range(n):
+#         if i == j:
+#             continue
+#         x1, y1 = points[i]
+#         x2, y2 = points[j]                        
 
-        euclid = pow(x2 - x1, 2) + pow(y2 - y1, 2) # не используем корень, чтобы не было float
-        dists[euclid] += 1 
-    for dist, cnt in dists.items():
-        ans += math.comb(cnt, 2)    
-print(ans)
+#         euclid = pow(x2 - x1, 2) + pow(y2 - y1, 2) # не используем корень, чтобы не было float
+#         dists[euclid] += 1 
+#     for dist, cnt in dists.items():
+#         ans += math.comb(cnt, 2)    
+# print(ans)
 
         
 
